bf1dea4366b05ef2e95a9f37265cc54fc6355a1a
damianhaq <40294540+damianhaq@users.noreply.github.com>
2022-07-30 12:05:03 +0200

    add button

--- src/components/inputPanel/InputPanel.js
+++ src/components/inputPanel/InputPanel.js
@@ -1,7 +1,7 @@
-import React from "react";
 import { addData } from "../../functions/addData";
 import Item from "./inputPanelComponent/Item";
 import styles from "./inputPanel.module.css";
+import { useState } from "react";
 
 const data = {
   repeat: {
@@ -23,14 +23,17 @@ const data = {
 };
 
 const InputPanel = () => {
+  const [inputData, setInputData] = useState({});
   // addData();
   return (
     <div className={styles.inputPanel}>
       <h2>Dodaj</h2>
       <Item name={data.repeat.name} data={data.repeat.pickNames} />
       <Item name={data.type.name} data={data.type.pickNames} />
+
       <Item name={data.name.name} data={data.name.pickNames} inputHeight={30} />
       <Item name={data.description.name} data={data.description.pickNames} inputHeight={70} />
+      <button>Dodaj</button>
     </div>
   );
 };
