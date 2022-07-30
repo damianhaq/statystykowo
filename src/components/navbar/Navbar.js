1d0f89bf182920258fcd441072fd31ced54606e0
damianhaq <40294540+damianhaq@users.noreply.github.com>
2022-07-30 14:28:14 +0200

    merge test

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./navbar.module.css";
 
-const navPick = ["Dodaj", "Statystykowo"];
+const navPick = ["Dodaj", "Statystykowo", "coś innego"];
 
 const Navbar = ({ nav, changeNav }) => {
   return (
