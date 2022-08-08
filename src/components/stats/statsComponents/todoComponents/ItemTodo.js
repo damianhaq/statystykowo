afa8678158888dbdef535e734d17f3da5c14b935
damianhaq <40294540+damianhaq@users.noreply.github.com>
2022-08-08 19:40:39 +0200

    rewrite ItemTodo comp

--- src/components/stats/statsComponents/todoComponents/ItemTodo.js
+++ src/components/stats/statsComponents/todoComponents/ItemTodo.js
@@ -1,110 +1,15 @@
 import React, { useEffect, useState } from "react";
-import CheckButton from "../../../CheckButton";
 import styles from "./itemTodo.module.css";
-import { Timestamp } from "firebase/firestore";
 
-const checkButtonsData = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-const checkButtonsDataYN = ["Tak", "Nie"];
-
-const initialData = {
-  date: Timestamp.fromDate(new Date()),
-  idTemplate: "",
-  type: "",
-  value: "",
-};
+import ItemTodoRender from "./ItemTodoRender";
 
 const ItemTodo = ({ event }) => {
-  const [data, setData] = useState(initialData);
-  // this number is for which one button is selected
-  const [isCheckedYN, setIsCheckedYN] = useState(0);
-  const [isChecked10, setIsChecked10] = useState(0);
-
-  useEffect(() => {
-    console.log("isChecked10", isChecked10);
-  }, [isChecked10]);
-
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-
-  function onClickHandlerYN(el, index) {
-    if (el == "Tak") {
-      setData({
-        date: Timestamp.fromDate(new Date()),
-        idTemplate: event.id,
-        type: event.event.type,
-        value: true,
-      });
-    } else if (el == "Nie") {
-      setData({
-        date: Timestamp.fromDate(new Date()),
-        idTemplate: event.id,
-        type: event.event.type,
-        value: false,
-      });
-    }
-
-    setIsCheckedYN(index + 1);
-  }
-
-  function onClickHandler10(el, index) {
-    console.log("onclick");
-    setData({
-      date: Timestamp.fromDate(new Date()),
-      idTemplate: event.id,
-      type: event.event.type,
-      value: index,
-    });
-    setIsChecked10(index + 1);
-  }
-  function onClickHandlerSave() {
-    console.log("onclick");
-  }
-
-  function Render({ event }) {
-    switch (event.event.type) {
-      case "Tak / Nie":
-        return (
-          <div className={styles.buttonsYNWrapper}>
-            {checkButtonsDataYN.map((el, index) => (
-              <CheckButton key={index} name={el} onClick={() => onClickHandlerYN(el, index)} isChecked={isCheckedYN == index + 1 ? true : false} />
-            ))}
-          </div>
-        );
-      case "Liczba":
-        return (
-          <input
-            value={data.value}
-            onChange={(ev) => setData({ date: Timestamp.fromDate(new Date()), idTemplate: event.id, type: event.event.type, value: ev.target.value })}
-            className={styles.inputNumber}
-            type="number"
-          />
-        );
-      case "Skala od-do":
-        return (
-          <div className={styles.buttonsYNWrapper}>
-            {checkButtonsData.map((el, index) => (
-              <CheckButton key={el} name={el} onClick={() => onClickHandler10(el, index)} isChecked={isChecked10 == index + 1 ? true : false} />
-            ))}
-          </div>
-        );
-      case "Tekst":
-        return <textarea className={styles.textarea} />;
-      case "Lista":
-        return <div>TODO: LISTA</div>;
-      default:
-        break;
-    }
-  }
-
   return (
     <div className={styles.itemTodo}>
       <h3>{event.event.name}</h3>
       <p className={styles.description}>{event.description}</p>
-      <Render event={event} />
-      <button onClick={onClickHandlerSave} className={styles.button}>
-        Zapisz
-      </button>
+      {/* <Render event={event} /> */}
+      <ItemTodoRender event={event} />
     </div>
   );
 };
---
+++ src/components/stats/statsComponents/todoComponents/ItemTodoRender.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import { Timestamp } from "firebase/firestore";
+import CheckButton from "../../../CheckButton";
+import styles from "./itemTodoRender.module.css";
+
+const checkButtonsData = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const checkButtonsDataYN = ["Tak", "Nie"];
+
+const initialData = {
+  date: Timestamp.fromDate(new Date()),
+  idTemplate: "",
+  type: "",
+  value: "",
+};
+
+const ItemTodoRender = ({ event }) => {
+  const [data, setData] = useState(initialData);
+  // this number is for which one button is selected
+  const [isCheckedYN, setIsCheckedYN] = useState(0);
+  const [isChecked10, setIsChecked10] = useState(0);
+
+  const value = "";
+
+  useEffect(() => {
+    console.log(data.value);
+  }, [data]);
+
+  function onClickHandlerYN(el, index) {
+    if (el == "Tak") {
+      setData({
+        date: Timestamp.fromDate(new Date()),
+        idTemplate: event.id,
+        type: event.event.type,
+        value: true,
+      });
+    } else if (el == "Nie") {
+      setData({
+        date: Timestamp.fromDate(new Date()),
+        idTemplate: event.id,
+        type: event.event.type,
+        value: false,
+      });
+    }
+
+    setIsCheckedYN(index + 1);
+  }
+
+  function onClickHandler10(el, index) {
+    console.log("onclick");
+    setData({
+      date: Timestamp.fromDate(new Date()),
+      idTemplate: event.id,
+      type: event.event.type,
+      value: index,
+    });
+    setIsChecked10(index + 1);
+  }
+
+  function onClickHandlerSave() {
+    console.log(data);
+  }
+
+  switch (event.event.type) {
+    case "Tak / Nie":
+      return (
+        <>
+          <div className={styles.buttonsYNWrapper}>
+            {checkButtonsDataYN.map((el, index) => (
+              <CheckButton key={index} name={el} onClick={() => onClickHandlerYN(el, index)} isChecked={isCheckedYN == index + 1 ? true : false} />
+            ))}
+          </div>
+          <button onClick={onClickHandlerSave} className={styles.button}>
+            Zapisz
+          </button>
+        </>
+      );
+    case "Liczba":
+      return (
+        <>
+          <input
+            value={data.value}
+            onChange={(ev) =>
+              setData({
+                date: Timestamp.fromDate(new Date()),
+                idTemplate: event.id,
+                type: event.event.type,
+                value: ev.target.value,
+              })
+            }
+            className={styles.inputNumber}
+            type="number"
+          />
+          <button onClick={onClickHandlerSave} className={styles.button}>
+            Zapisz
+          </button>
+        </>
+      );
+    case "Skala od-do":
+      return (
+        <>
+          <div className={styles.buttonsYNWrapper}>
+            {checkButtonsData.map((el, index) => (
+              <CheckButton key={el} name={el} onClick={() => onClickHandler10(el, index)} isChecked={isChecked10 == index + 1 ? true : false} />
+            ))}
+          </div>
+          <button onClick={onClickHandlerSave} className={styles.button}>
+            Zapisz
+          </button>
+        </>
+      );
+    case "Tekst":
+      return (
+        <>
+          <textarea
+            value={data.value}
+            onChange={(ev) =>
+              setData({
+                date: Timestamp.fromDate(new Date()),
+                idTemplate: event.id,
+                type: event.event.type,
+                value: ev.target.value,
+              })
+            }
+            className={styles.textarea}
+          />
+          <button onClick={onClickHandlerSave} className={styles.button}>
+            Zapisz
+          </button>
+        </>
+      );
+    case "Lista":
+      return (
+        <>
+          <div>TODO: LISTA</div>
+          <button onClick={onClickHandlerSave} className={styles.button}>
+            Zapisz
+          </button>
+        </>
+      );
+    default:
+      break;
+  }
+};
+
+export default ItemTodoRender;
