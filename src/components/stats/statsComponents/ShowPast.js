d5fb694b72e0cab29917264dba46b4e78e9eaa26
damianhaq <40294540+damianhaq@users.noreply.github.com>
2022-08-27 11:24:34 +0200

    filter by one month and arrow icon

--- src/components/stats/statsComponents/ShowPast.js
+++ src/components/stats/statsComponents/ShowPast.js
@@ -3,27 +3,31 @@ import { useEffect } from "react";
 import { useState } from "react";
 import styles from "./showPast.module.css";
 
+import angleLeft from "../../../icons/angle-left-solid.svg";
+import angleRight from "../../../icons/angle-right-solid.svg";
+
 const months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
 
 const ShowPast = ({ done, events }) => {
   const [doneFilteredByMonth, setDoneFilteredByMonth] = useState([]);
-  const [pickMonth, setPickMonth] = useState({ month: new Date().getMonth(), year: new Date().getFullYear() });
+  const [pickMonth, setPickMonth] = useState(new Date().getMonth());
+  // const [pickMonth, setPickMonth] = useState({ month: new Date().getMonth(), year: new Date().getFullYear() });
 
   useEffect(() => {
     // Get only current/picked month from "done"
-    const tempArr = done.filter((el) => new Date(el.done.date.seconds * 1000).getMonth() == pickMonth.month); // (January gives 0) thats why now its 7
+    const tempArr = done.filter((el) => new Date(el.done.date.seconds * 1000).getMonth() == pickMonth); // (January gives 0) thats why now its 7
     setDoneFilteredByMonth(tempArr);
-  }, []);
+  }, [pickMonth]);
 
   return (
     <div className={styles.showPast}>
       <h2>Statystyki</h2>
       <div className={styles.monthWrapper}>
         {/* TODO: its not refreshing after click */}
-        <button onClick={() => setPickMonth((prev) => ({ ...prev, month: 2 }))}>pop</button>
+        <img src={angleLeft} onClick={() => setPickMonth((prev) => prev - 1)}></img>
         {/* Show name of month */}
-        {doneFilteredByMonth.length > 0 && <h3>{months[new Date(doneFilteredByMonth[1].done.date.seconds * 1000).getMonth()]}</h3>}
-        <button>nas</button>
+        <h3>{months[pickMonth]}</h3>
+        <img src={angleRight} onClick={() => setPickMonth((prev) => prev + 1)}></img>
       </div>
       {doneFilteredByMonth.map((el) => (
         <div className={styles.item} key={el.id}>
